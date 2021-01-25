4561127b5ccb52aafcc2f074fcd80afd945aabdb
abh224272 <abh224272@gmail.com>
2021-01-25 22:31:30 +0530

    [Abhishek Mishra] Add: Created Connection to Address Book Database using SQL Connection

---
+++ AddressBookSystem/AddressBookRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class AddressBookRepo
+    {
+        public static string connectionString = "Server=(Localdb)\\MSSQLLocalDB;database=AddressBookServiceDB;Trusted_Connection=true";
+        SqlConnection sqlConnection = new SqlConnection(connectionString);
+        
+    }
+}
