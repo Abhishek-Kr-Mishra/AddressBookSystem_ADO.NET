520225f9bbbecd5daf01c456ba1dcbb45df59410
abh224272 <abh224272@gmail.com>
2021-01-25 22:38:40 +0530

    [Abhishek Mishra] Add: Created Attributes of Address Book

---
+++ AddressBookSystem/AddressBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class AddressBook
+    {
+        public int BookID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PersonAddress { get; set; }
+        public long PhoneNumber { get; set; }
+        public string EmailID { get; set; }
+        public int CountryID { get; set; }
+        public int StateID { get; set; }
+        public int CityID { get; set; }
+        public int ZipID { get; set; }
+        public int PersonTypeID { get; set; }
+    }
+}
