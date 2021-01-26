ff19d107bba8a2a85648a07b1cbc603a678ba81a
abh224272 <abh224272@gmail.com>
2021-01-26 12:34:59 +0530

    [Abhishek Mishra] Add: Created method to insert address into Address Book table

--- AddressBookSystem/AddressBookRepo.cs
+++ AddressBookSystem/AddressBookRepo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Text;
 
@@ -9,6 +10,41 @@ namespace AddressBookSystem
     {
         public static string connectionString = "Server=(Localdb)\\MSSQLLocalDB;database=AddressBookServiceDB;Trusted_Connection=true";
         SqlConnection sqlConnection = new SqlConnection(connectionString);
-        
+        public bool InseertAddressToAddressBook(AddressBook addressBook)
+        {
+            try
+            {
+                using (this.sqlConnection)
+                {
+                    SqlCommand command = new SqlCommand("insertAddress", sqlConnection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@FirstName", addressBook.FirstName);
+                    command.Parameters.AddWithValue("@LastName", addressBook.LastName);
+                    command.Parameters.AddWithValue("@PersonAddress", addressBook.PersonAddress);
+                    command.Parameters.AddWithValue("@PhoneNumber", addressBook.PhoneNumber);
+                    command.Parameters.AddWithValue("@EmailID", addressBook.EmailID);
+                    command.Parameters.AddWithValue("@CountryID", addressBook.CountryID);
+                    command.Parameters.AddWithValue("@StateID", addressBook.StateID);
+                    command.Parameters.AddWithValue("@CityID", addressBook.CityID);
+                    command.Parameters.AddWithValue("@ZipID", addressBook.ZipID);
+                    command.Parameters.AddWithValue("@PersonTypeID", addressBook.PersonTypeID);
+                    this.sqlConnection.Open();
+                    var result = command.ExecuteNonQuery();
+                    if(result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }       
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                this.sqlConnection.Close();
+            }
+        }
     }
 }
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -6,7 +6,24 @@ namespace AddressBookSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AddressBook addressBook = new AddressBook();
+            AddressBookRepo addressBookRepo = new AddressBookRepo();
+
+            addressBook.FirstName = "Aditya";
+            addressBook.LastName = "Mishra";
+            addressBook.PersonAddress = "05 Florida Street";
+            addressBook.PhoneNumber = 7896540123;
+            addressBook.EmailID = "adi@gmail.com";
+            addressBook.CountryID = 4;
+            addressBook.StateID = 4;
+            addressBook.CityID = 2;
+            addressBook.ZipID = 11254;
+            addressBook.PersonTypeID = 2;
+
+            if(addressBookRepo.InseertAddressToAddressBook(addressBook))
+            {
+                Console.WriteLine("Address Inserted Succesfully");
+            }
         }
     }
 }
